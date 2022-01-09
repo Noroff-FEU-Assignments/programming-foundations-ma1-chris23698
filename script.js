1384d25e52cd0d8e55664c48e787cdd8863fd8ff
chris23698 <chris23698@gmail.com>
2022-01-09 22:41:29 +0100

    MA1

---
+++ script.js
@@ -0,0 +1,28 @@
+// task 1 //
+var firstVar = "";
+// task 2 //
+var name = "Christoffer";
+console.log(name)
+// task 3 //
+var number = 8;
+console.log(number)
+// task 4 //
+var initialise = 20/5;
+console.log(initialise)
+// task 5 //
+var type = typeof("frog");
+console.log(type);
+// task 6 //
+var orderHasShipped = true;
+console.log(orderHasShipped)
+// task 7 //
+if (orderHasShipped === true) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+// task 8 //
+for (var counter = 0; 
+    counter < 10; counter++) {
+        console.log(counter)
+    }
